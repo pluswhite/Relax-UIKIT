bdf8e7f7dd1b786a3feb97659856dc69648f048e
EricThsi <beefjava@gmail.com>
2020-07-15 22:42:49 +0800

    feat(components): add button component for testing

---
+++ src/compoments/Button/__test__/button.test.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Button, { ButtonProps } from '../button';
+
+const defaultProps = {
+  onClick: jest.fn(),
+};
+
+const testProps: ButtonProps = {
+  btnType: 'primary',
+  size: 'lg',
+  className: 'test-btn',
+};
+
+const disabledProps: ButtonProps = {
+  disabled: true,
+  onClick: jest.fn(),
+};
+
+describe('Button component test', () => {
+  it('should render the default button correctly', () => {
+    const wrapper = render(<Button {...defaultProps}>Default</Button>);
+    const elm = wrapper.getByText('Default');
+
+    expect(elm).toBeInTheDocument();
+    expect(elm.tagName).toEqual('BUTTON');
+    expect(elm).toHaveClass('dns-button dns-button--default');
+
+    fireEvent.click(elm);
+    expect(defaultProps.onClick).toHaveBeenCalled();
+  });
+
+  it('should render the correct button based on different props', () => {
+    const wrapper = render(<Button {...testProps}>Button</Button>);
+    const elm = wrapper.getByText('Button');
+
+    expect(elm).toBeInTheDocument();
+    expect(elm).toHaveClass(
+      'dns-button dns-button--primary dns-button--lg test-btn',
+    );
+  });
+
+  it('should render disabled button correctly when disabled prop set to be true', () => {
+    const wrapper = render(<Button {...disabledProps}>Disabled</Button>);
+    const elm = wrapper.getByText('Disabled') as HTMLButtonElement;
+
+    expect(elm).toBeInTheDocument();
+    expect(elm).toHaveClass('dns-button dns-button--default');
+    expect(elm).toHaveProperty('disabled');
+    expect(elm.disabled).toBeTruthy();
+
+    fireEvent.click(elm);
+    expect(disabledProps.onClick).not.toHaveBeenCalled();
+  });
+});
---
+++ src/compoments/Button/button.tsx
@@ -0,0 +1,105 @@
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react';
+import classnames from 'classnames';
+
+import './button.scss';
+
+interface IBaseButtonProps {
+  className?: string;
+  disabled?: boolean;
+  btnType?:
+    | 'default'
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'warning'
+    | 'info'
+    | 'danger'
+    | 'link';
+  size?: 'xs' | 'sm' | 'md' | 'lg';
+  plain?: boolean;
+  round?: boolean;
+  circle?: boolean;
+  href?: string;
+  children?: React.ReactNode;
+  onClick?: (
+    e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>,
+  ) => void;
+}
+
+type NativeButtonProps = IBaseButtonProps & ButtonHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = IBaseButtonProps & AnchorHTMLAttributes<HTMLElement>;
+
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
+
+export const Button: FC<ButtonProps> = (props: ButtonProps) => {
+  const {
+    btnType = 'default',
+    className,
+    disabled,
+    size,
+    plain,
+    round,
+    circle,
+    href,
+    children,
+    onClick,
+    ...restProps
+  } = props;
+
+  const isLink = btnType === 'link';
+
+  const classes = classnames(
+    'dns-button',
+    `dns-button--${btnType}`,
+    {
+      [`dns-button--${size}`]: size,
+      'is-plain': plain,
+      'is-disabled': disabled,
+      'is-round': round,
+      'is-circle': circle,
+    },
+    className,
+  );
+
+  const handleClick = (
+    evt: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>,
+  ) => {
+    onClick && onClick(evt);
+  };
+
+  if (isLink) {
+    return (
+      <a
+        className={classes}
+        href={href || '#'}
+        {...restProps}
+        onClick={handleClick}
+      >
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <button
+      className={classes}
+      disabled={disabled}
+      {...restProps}
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+interface ButtonGroupProps {
+  children: React.ReactChild;
+}
+
+export const ButtonGroup: FC<ButtonGroupProps> = ({
+  children,
+}: ButtonGroupProps) => {
+  return <div className="dns-button-group">{children}</div>;
+};
+
+export default Button;
