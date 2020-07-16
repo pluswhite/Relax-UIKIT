0e9e04fc14734107018213da1b73b2d1bd1d4f5d
EricThsi <beefjava@gmail.com>
2020-07-16 09:09:28 +0800

    feat(entry): add default export in entry file

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,4 @@
 export { default as Button } from './components/Button';
+export { default as Radio } from './components/Radio';
+export { default as Checkbox } from './components/Checkbox';
+export { default as Input } from './components/Input';
