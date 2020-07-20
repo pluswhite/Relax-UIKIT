7b61deeddda3e72bea1a58c62826d80d9af2b0ed
EricThsi <beefjava@gmail.com>
2020-07-20 21:57:24 +0800

    feat(icons): update icons stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,7 @@
 import '!style-loader!css-loader!sass-loader!../src/index.scss';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas, faBell } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, fas);
