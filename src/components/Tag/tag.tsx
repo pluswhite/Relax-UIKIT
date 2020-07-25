94730b93032cc56c4a19fc0c863e4fd4998ab3f0
EricThsi <beefjava@gmail.com>
2020-07-25 22:45:46 +0800

    feat(tag): update tag theme

--- src/components/Tag/tag.tsx
+++ src/components/Tag/tag.tsx
@@ -2,32 +2,58 @@ import React, { FC, ReactChildren, ReactElement } from 'react';
 import classnames from 'classnames';
 
 import Icon from '../Icon';
+import { tuple } from '../../util/colors';
 
 import './tag.scss';
 
+const PresetColorTypes = tuple(
+  'primary',
+  'secondary',
+  'success',
+  'warning',
+  'danger',
+  'info',
+  'default',
+);
+
 export interface ITagProps {
-  color?: ThemeType | string;
+  color?: ThemeType | 'default' | string;
   className?: string;
   closable?: boolean;
   onClose?: () => void;
+  style?: React.CSSProperties;
   children: ReactChildren | ReactElement;
 }
 
+const PresetColorRegex = new RegExp(`^${PresetColorTypes.join('|')}`);
+
 const Tag: FC<ITagProps> = (props: ITagProps) => {
-  const { color, closable, className, children, ...restProps } = props;
+  const { color = 'default', closable, onClose, className, children, style, ...restProps } = props;
+  const isPresetColor = (): boolean => {
+    if (!color) {
+      return false;
+    }
+
+    return PresetColorRegex.test(color);
+  };
   const classes = classnames(
     'rx-tag',
-    `rx-tag--${color}`,
     {
+      [`rx-tag--${color}`]: isPresetColor(),
       'is-closable': closable,
     },
     className,
   );
 
+  const styles = {
+    backgroundColor: color && !isPresetColor() ? color : undefined,
+    ...style,
+  };
+
   return (
-    <span className={classes} {...restProps}>
+    <span className={classes} {...restProps} style={styles}>
       {children}
-      {closable && <Icon icon="times" theme="dark" />}
+      {closable && <Icon icon="times" onClick={onClose} />}
     </span>
   );
 };
