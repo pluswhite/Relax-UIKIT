39b8cc384d038cfcebb1ef3771a9fe997794a9e8
EricThsi <beefjava@gmail.com>
2020-08-09 22:24:16 +0800

    feat(link): update styling of link

--- src/components/Link/link.tsx
+++ src/components/Link/link.tsx
@@ -5,13 +5,21 @@ import './link.scss';
 
 export interface ILinkProps {
   type?: ThemeType;
+  disabled?: boolean;
   className?: string;
   children: ReactChildren | ReactElement;
 }
 
 const Link: FC<ILinkProps> = (props: ILinkProps) => {
-  const { className, children, ...restProps } = props;
-  const classes = classnames('rx-link', className);
+  const { type, disabled, className, children, ...restProps } = props;
+  const classes = classnames(
+    'rx-link',
+    {
+      [`rx-link--${type}`]: type,
+      [`is-disabled`]: disabled,
+    },
+    className,
+  );
 
   return (
     <a className={classes} {...restProps}>
